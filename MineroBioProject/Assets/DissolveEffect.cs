f04e0720cb13a2800d093f949caf69e1e4f427c3
Eskil <eskil.torland@gmail.com>
2020-01-30 09:51:12 +0100

    Start implementation of Dissolve Effect

---
+++ MineroBioProject/Assets/DissolveEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveEffect : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    private float dissolveAmount;
+    private bool isDissolving;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDissolving)
+        {
+            dissolveAmount = Mathf.Clamp01(dissolveAmount + Time.deltaTime);
+            material.SetFloat("_DissolveAmount", dissolveAmount);
+        } else
+        {
+            dissolveAmount = Mathf.Clamp01(dissolveAmount - Time.deltaTime);
+            material.SetFloat("_DissolveAmount", dissolveAmount);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            isDissolving = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            isDissolving = false;
+        }
+    }
+}
