816302911d8343cb209c4007432b643923b00481
Anders <andesob@stud.ntnu.no>
2020-02-04 22:09:49 +0100

    Cleaned up some code

---
+++ MineroBioProject/Assets/PlayerPickupScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickupScript : MonoBehaviour
+{
+
+    public Transform gun;
+    private bool gunPickedUp;
+
+    public void PickUpGun()
+    {
+        gun.gameObject.SetActive(true);
+        gunPickedUp = true;
+    }
+
+    public bool hasGun()
+    {
+        return gunPickedUp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Gun"))
+        {
+            PickUpGun();
+        }
+    }
+
+}
