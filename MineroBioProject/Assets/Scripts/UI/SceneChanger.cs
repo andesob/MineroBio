f1d76d6cfbcdb997422e6c18018413a8ea4bfcdc
Anders <andesob@stud.ntnu.no>
2020-02-14 16:17:27 +0100

    Added a SceneController script
    The SceneController scripts keeps track of info when changing scenes. This is to let the character spawn in different locations.

---
+++ MineroBioProject/Assets/Scripts/UI/SceneChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    [SerializeField] private string toScene;
+    private SceneController changeLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeLevel = GameObject.FindGameObjectWithTag("GameController").GetComponent<SceneController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            changeLevel.LoadScene(toScene);
+        }
+    }
+}
