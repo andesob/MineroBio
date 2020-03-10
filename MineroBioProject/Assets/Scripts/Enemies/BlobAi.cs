a2b0add439c783b1c18d5ccff8bb637f6c6e9b02
Eskil <eskil.torland@gmail.com>
2020-03-10 12:34:09 +0100

    Made Doors and blob attack

---
+++ MineroBioProject/Assets/Scripts/Enemies/BlobAi.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobAi : MonoBehaviour
+{
+    public enum State
+    {
+        roaming,
+        chase,
+        shoot,
+        goingBackToStart
+
+    }
+
+    private EnemyMovement enemyMovement;
+    public GameObject player;
+    private Vector3 startPos;
+    private State state;
+
+    public float detectionDistance;
+    public float maxDistance;
+
+   
+
+    private void Awake()
+    {
+        enemyMovement = this.gameObject.GetComponent<EnemyMovement>();
+        state = State.chase;
+        startPos = transform.position;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   /* private void FixedUpdate()
+    {
+        switch (state)
+        {
+            case State.roaming:
+                if(Vector3.Distance(startPos,enemyMovement.GetPlayerPosition()) < detectionDistance)
+                {
+                    state = State.chase;
+                }
+                break;
+
+            case State.chase:
+                if(player.transform.position.x - this.gameObject.transform.position.x > maxDistance || transform.position.y > maxDistance)
+                {
+
+                }
+
+        }
+    }*/
+}
