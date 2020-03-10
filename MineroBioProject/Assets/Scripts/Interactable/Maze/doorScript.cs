4f022f2c89cf8eed9892a44fcd7f012c789422f0
Trym <trymj@stud.ntnu.no>
2020-03-10 12:30:40 +0100

    Added mazeinteractions with levers and "doors"

---
+++ MineroBioProject/Assets/Scripts/Interactable/Maze/doorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorScript : MonoBehaviour
+{
+    [SerializeField] GameObject doorOpen;
+    [SerializeField] GameObject doorClosed;
+
+    public bool isOpen;
+
+    // sets the state of the door according to what the isOpen bool says.
+    private void Start()
+    {
+        if (isOpen)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = doorOpen.GetComponent<SpriteRenderer>().sprite;
+        }
+        else if (!isOpen)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = doorClosed.GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+
+    // changes the state of the door.
+    public void changeState()
+    {
+        if (isOpen)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = doorClosed.GetComponent<SpriteRenderer>().sprite;
+            isOpen = false;
+        }
+        else if (!isOpen)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = doorOpen.GetComponent<SpriteRenderer>().sprite;
+            isOpen = true;
+        }
+    }
+}
