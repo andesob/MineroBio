2fda63e698422a85f32654ed363c385663dfd545
rojahno <andrersu@ntnu.no>
2020-03-17 21:10:08 +0100

    Improved DeathUI
    Made it similar to the pauseMenu

--- MineroBioProject/Assets/Scripts/UI/DeathButtonClick.cs
+++ MineroBioProject/Assets/Scripts/UI/DeathButtonClick.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class DeathMenu : MonoBehaviour
+public class DeathButtonClick : MonoBehaviour
 {
     public void RetryButton(string scene)
     {
