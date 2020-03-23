c91fe732f24ac89c3dcd65e9a050b384e1eaad1f
Anders <andesob@stud.ntnu.no>
2020-03-23 21:00:00 +0100

    Fixed sniperbulletscript and added tutorial level

--- MineroBioProject/Assets/Scripts/Combat/Bullet.cs
+++ MineroBioProject/Assets/Scripts/Combat/Bullet.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float fireRate = 2;
-
     public GameObject hitPrefab;
     public GameObject muzzlePrefab;
     public GameObject tip;
@@ -13,70 +12,62 @@ public class Bullet : MonoBehaviour
     private float time;
     private int PISTOL_DAMAGE = 50;
 
-    void Start()
-    {
-
-        if (muzzlePrefab != null)
-        {
-
-            var muzzleVFX = Instantiate(muzzlePrefab, transform.position, transform.rotation);
-
+    void Start()
+    {
+        if (muzzlePrefab != null)
+        {
+            var muzzleVFX = Instantiate(muzzlePrefab, transform.position, transform.rotation);
             var psMuzzle = muzzleVFX.GetComponent<ParticleSystem>();
-            if (psMuzzle != null)
-            {
-                Destroy(muzzleVFX, psMuzzle.main.duration);
-            }
-            else
-            {
-                var psChild = muzzleVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
-                Destroy(muzzleVFX, psChild.main.duration);
-            }
+            if (psMuzzle != null)
+            {
+                Destroy(muzzleVFX, psMuzzle.main.duration);
+            }
+            else
+            {
+                var psChild = muzzleVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(muzzleVFX, psChild.main.duration);
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-
+    {
         if (!(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Melee")) && Time.time > time)
         {
             BulletHit(collision.gameObject);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         if (!(collision.CompareTag("Player") || collision.CompareTag("Melee")) && Time.time > time)
         {
             BulletHit(collision.gameObject);
         }
     }
 
-    private void BulletHit(GameObject collision)
-    {
+    private void BulletHit(GameObject collision)
+    {
         time = Time.time + 1;
         Vector2 pos = tip.transform.position;
         Quaternion rot = Quaternion.Euler(tip.transform.rotation.x, tip.transform.rotation.y, tip.transform.rotation.z + 90f);
-
-        if (hitPrefab != null)
-        {
-            var hitVFX = Instantiate(hitPrefab, pos, rot);
-            var psHit = hitVFX.GetComponent<ParticleSystem>();
-            if (psHit != null)
-            {
-                Destroy(hitVFX, psHit.main.duration);
-            }
-            {
-                var psChild = hitVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
-                Destroy(hitVFX, psChild.main.duration);
-            }
+        if (hitPrefab != null)
+        {
+            var hitVFX = Instantiate(hitPrefab, pos, rot);
+            var psHit = hitVFX.GetComponent<ParticleSystem>();
+
+            if (psHit != null)
+            {
+                Destroy(hitVFX, psHit.main.duration);
+            }
+
+            var psChild = hitVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+            Destroy(hitVFX, psChild.main.duration);
         }
-
-        if (collision.CompareTag("Enemy"))
-        {
-            collision.GetComponent<EnemyAi>().TakeDamage(PISTOL_DAMAGE);
-        }
-
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<EnemyAi>().TakeDamage(PISTOL_DAMAGE);
+        }
         Destroy(gameObject);
     }
 }
--- MineroBioProject/Assets/Scripts/Combat/Enemies/EnemyMovement.cs
+++ MineroBioProject/Assets/Scripts/Combat/Enemies/EnemyMovement.cs
@@ -63,7 +63,7 @@ public class EnemyMovement : MonoBehaviour
     public void MoveEnemyAwayPlayer()
     {
         rb.MovePosition((Vector2)transform.position - (playerDirection * movementSpeed * Time.deltaTime));
-        if (this.gameObject.transform.position.x > follewedPlayer.transform.position.x + 0.1f)
+        if (this.gameObject.transform.position.x > mainCharacter.transform.position.x + 0.1f)
         {
             this.gameObject.transform.rotation = Quaternion.Euler(0.0f, 180f, 0.0f);
         }
